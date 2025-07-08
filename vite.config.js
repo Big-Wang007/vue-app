e373efa1a892db459a5d2b717cac9e069d9d6100
Big-Wang007 <wsguangya@163.com>
2025-07-08 23:22:02 +0800

    feat: github pages专用配置

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,49 @@ import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 // import VueDevTools from "vite-plugin-vue-devtools";
 import qiankun from "vite-plugin-qiankun";
-import { qiankunWindow } from "vite-plugin-qiankun/dist/helper";
+// import { qiankunWindow } from "vite-plugin-qiankun/dist/helper";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    base: qiankunWindow.__POWERED_BY_QIANKUN__ ? "/vue-app/" : "/",
+    base: "/vue-app/",
     plugins: [vue(),/*  VueDevTools(), */ qiankun("vue-app", { useDevMode: true })],
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    build: {
+      outDir: "dist",
+      sourcemap: false,
+      // 消除打包大小超过500kb警告
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        // input: {
+        //   index: index.html,
+        // },
+        output: {
+          chunkFileNames: "assets/[name]-[hash].js",
+          entryFileNames: "assets/[name]-[hash].js",
+          assetFileNames: "assets/[name]-[hash].[ext]",
+          // TODO: 处理GitHub Pages 部署 _plugin-vue_export-helper.js 404
+          // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+          sanitizeFileName(name) {
+            const match = DRIVE_LETTER_REGEX.exec(name);
+            const driveLetter = match ? match[0] : "";
+            // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+            // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+            return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "");
+          },
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return (
+                id.toString().match(/\/node_modules\/(?!.pnpm)(?<moduleName>[^\/]*)\//)?.groups.moduleName ?? "vender"
+              );
+            }
+          },
+        },
       },
     },
     server: {
